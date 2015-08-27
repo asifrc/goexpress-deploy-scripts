005d17335a11d8339d544b7741e70d447bfac39d
Asif R Choudhury <choudhury.asif@gmail.com>
2015-08-27 11:50:55 -0500

    Switching to  colon

--- runChefClient.ps1
+++ runChefClient.ps1
@@ -1,1 +1,1 @@
-Invoke-Command -ScriptBlock {mkdir C:\AsifWasHere} -ComputerName %server% -Credential $(New-Object System.Management.Automation.PSCredential %username%, $(ConvertTo-SecureString -AsPlainText -Force %password%))
+Invoke-Command -ScriptBlock {mkdir C:\AsifWasHere} -ComputerName $env:server -Credential $(New-Object System.Management.Automation.PSCredential $env:username, $(ConvertTo-SecureString -AsPlainText -Force $env:password))
