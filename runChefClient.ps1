a69c3147774cf85b4c105558fb01e20833545781
Asif R Choudhury <choudhury.asif@gmail.com>
2015-08-27 10:33:42 -0500

    Added stuff in it

---
+++ runChefClient.ps1
@@ -0,0 +1,1 @@
+Invoke-Command -ScriptBlock {mkdir C:\AsifWasHere} -ComputerName $server -Credential $(New-Object System.Management.Automation.PSCredential $username, $(ConvertTo-SecureString -AsPlainText -Force $password))
