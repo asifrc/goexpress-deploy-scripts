6ce92ce73933a22b7e969e5ff9cfb282629171fb
Asif R Choudhury <choudhury.asif@gmail.com>
2015-08-27 11:48:13 -0500

    Switch to percents for env variables

--- runChefClient.ps1
+++ runChefClient.ps1
@@ -1,1 +1,1 @@
-Invoke-Command -ScriptBlock {mkdir C:\AsifWasHere} -ComputerName $server -Credential $(New-Object System.Management.Automation.PSCredential $username, $(ConvertTo-SecureString -AsPlainText -Force $password))
+Invoke-Command -ScriptBlock {mkdir C:\AsifWasHere} -ComputerName %server% -Credential $(New-Object System.Management.Automation.PSCredential %username%, $(ConvertTo-SecureString -AsPlainText -Force %password%))
